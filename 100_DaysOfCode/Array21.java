269a53e8712bcc71f6b1480240f9330826ff75fe
Dhanush <dhanush12212@gmail.com>
2025-09-26 19:23:51 +0530

    Day 32

--- 100_DaysOfCode/Array21.java
+++ 100_DaysOfCode/Array21.java
@@ -1,30 +1,66 @@
-import java.util.*;
+// 3 Sum problem
 
-// 3 Sum (Better Approach using Hashset)
+import java.util.*;
 
 public class Array21 {
 
-    public static List<List<Integer>> triplet(int n, int arr[]) {
-        Set<List<Integer>> st = new HashSet<>();
+    // Better Approach using Hashset
+
+    // public static List<List<Integer>> triplet(int n, int nums[]) {
+    //     Set<List<Integer>> st = new HashSet<>();
+    //     for(int i=0; i<n; i++) {
+    //         Set<Integer> hashset = new HashSet<>();
+    //         for(int j=i+1; j<n; j++) {
+    //             int third = -(nums[i] + nums[j]);
+    //             if(hashset.contains( third )) {
+    //                 List<Integer> temp = Arrays.asList(nums[i], nums[j], third);
+    //                 temp.sort(null);
+    //                 st.add(temp);
+    //             }
+    //             hashset.add(nums[j]);
+    //         }
+    //     }
+    //     return new ArrayList<>(st);
+    // }
+
+
+    //Optimal Solution (Using 3 pointers)
+
+    public static List<List<Integer>> triplet(int n, int nums[]) {
+        Set<List<Integer>> st = new HashSet<>(); 
+        Arrays.sort(nums);
         for(int i=0; i<n; i++) {
-            Set<Integer> hashset = new HashSet<>();
-            for(int j=i+1; j<n; j++) {
-                int third = -(arr[i] + arr[j]);
-                if(hashset.contains( third )) {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
-                    temp.sort(null);
+            
+            if( i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            
+            while( j < k ) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if(sum < 0) 
+                j++;
+                else if(sum > 0) 
+                k--;
+                else {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
                     st.add(temp);
+                    j++;
+                    k--;
+
+                    while( j < k && nums[j] == nums[j-1]) j++; 
+                    while( j < k && nums[k] == nums[k+1]) k--; 
                 }
-                hashset.add(arr[j]);
             }
         }
         return new ArrayList<>(st);
     }
 
     public static void main(String[] args) {
-        int[] arr = { -1, 0, 1, 2, -1, -4};
-        int n = arr.length;
-        List<List<Integer>> ans = triplet(n, arr);
+        int[] nums = { -1, 0, 1, 2, -1, -4};
+        int n = nums.length;
+        List<List<Integer>> ans = triplet(n, nums);
         for (List<Integer> it : ans) {
             System.out.print("[");
             for (Integer i : it) {
