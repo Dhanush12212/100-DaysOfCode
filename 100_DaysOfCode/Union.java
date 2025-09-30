b975769fbb7bdee1e4e95987a82d7da627a18a25
Dhanush <dhanush12212@gmail.com>
2025-09-30 10:16:51 +0530

    Day 35

---
+++ 100_DaysOfCode/Union.java
@@ -0,0 +1,68 @@
+//Union of two sorted array
+import java.util.*;
+
+public class Union {
+
+//Better Approach( Using HashSet )
+
+// public static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+//     HashSet<Integer> set = new HashSet<>();
+//     ArrayList<Integer> union = new ArrayList<>();
+
+//     for(int it : arr1)
+//         set.add(it);
+
+//     for(int it : arr2)
+//         set.add(it);
+
+//     for(int it : set)
+//         union.add(it);
+
+//     return union;
+// }
+
+
+//Optimal Solution (Two Pointer Approach)
+
+    public static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+        ArrayList<Integer> union = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+
+        while(i < n && j < m) {
+            if(arr1[i] <= arr2[j]) {
+                if(union.size() == 0 || union.get(union.size() -1) != arr1[i]) 
+                    union.add(arr1[i]);
+                i++;
+            } else {
+                if(union.get(union.size() -1) != arr2[j]) 
+                    union.add(arr2[j]);
+                j++;
+            } 
+        }
+
+        while( i < n ) {
+            if(union.get(union.size() -1) != arr1[i]) 
+                union.add(arr1[i]);
+            i++;
+        }
+
+        while( j < m ) {
+            if(union.size() == 0 || union.get(union.size() -1) != arr2[j]) 
+                union.add(arr2[j]);
+            j++;
+        }
+
+        return union;
+    }
+
+    public static void main(String args[]) {
+        int n = 10, m = 7;
+        int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+        ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
+        System.out.println("Union of arr1 and arr2 is ");
+        for (int val: Union)
+          System.out.print(val+" ");
+    }
+}
